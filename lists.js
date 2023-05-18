a5778fc882c727b6354562f0662585e1efee33a7
Cypresskids10 <134014282+Cypresskids10@users.noreply.github.com>
2023-05-18 17:09:34 -0500

    Add files via upload

---
+++ lists.js
@@ -0,0 +1,101 @@
+export let blockArray = [
+    "./items/dirt.png",//0
+    "./items/stone.png",//1
+    "./items/coal_ore.webp",//2
+    "./items/iron_ore.webp",//3
+    "./items/lapis_ore.webp",//4
+    "./items/gold_ore.webp",//5
+    "./items/redstone_ore.webp",//6
+    "./items/diamond_ore.webp",//7
+    "./items/emerald_ore.webp",//8
+    "./items/obsidian.png",//9
+    "./items/sculk.webp",//10
+    "./items/netherrack.png",//11
+    "./items/magma_block.png",//12
+    "./items/nether_quartz_ore.webp",//13
+    "./items/nether_gold_ore.webp",//14
+    "./items/ancient_debris.png",//15
+    "./items/end_stone.png",//16
+    "./items/purpur_block.png",//17
+    "./items/bedrock.png",//18
+];
+
+export let pickaxeArray = [
+    "./items/wooden_pickaxe.png",//0
+    "./items/stone_pickaxe.png",//1
+    "./items/iron_pickaxe.png",//2
+    "./items/lapiz_pickaxe.png",//3
+    "./items/golden_pickaxe.png",//4
+    "./items/redstone_pickaxe.png",//5
+    "./items/diamond_pickaxe.png",//6
+    "./items/emerald_pickaxe.png",//7
+    "./items/obsidian_pickaxe.png",//8
+    "./items/skulk_pickaxe.png",//9
+    "./items/netherack_pickaxe.png",//10
+    "./items/quartz_pickaxe.png",//11
+    "./items/nether_brick_pickaxe.png",//12
+    "./items/blazing_pickaxe.webp",//13
+    "./items/wither_bone_pickaxe.png",//14
+    "./items/netherite_pickaxe.png",//15
+    "./items/end_stone_pickaxe.png",//16
+    "./items/end_crystal_pickaxe.png",//17
+    "./items/dragon_pickaxe.png",//18
+    "./items/shulker_pickaxe.png",//19
+    "./items/bedrock_pickaxe.png",//20
+];
+
+export let dimensionList = [
+    "./dimensions/overworld.webp",
+    "./dimensions/nether.avif",
+    "./dimensions/end.webp",
+];
+
+export let fortuneList = [
+    10000,
+    100000,
+    1000000,
+    "Max Level"
+];
+
+export let efficiencyList = [
+    5000,
+    20000,
+    100000,
+    5000000,
+    1000000000,
+    "Max Level"
+];
+
+export let rarityList = [
+    10000,
+    1000000,
+    1000000000,
+    "Max Level"
+];
+
+export let farmerList = [
+    500,
+    5000,
+    12500,
+    30000,
+    100000,
+    "Max Level"
+];
+
+export let blacksmithList = [
+    2500,
+    10000,
+    65000,
+    112000,
+    565000,
+    "Max Level"
+];
+
+export let librarianList = [
+    15000,
+    85000,
+    735000,
+    2650000,
+    75000000,
+    "Max Level"
+];
